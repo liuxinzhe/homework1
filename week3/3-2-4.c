aea3125eba58c2fbc9fdf9b1b9b94ee655bc8562
liuxinzhe <72716124+liuxinzhe@users.noreply.github.com>
2020-10-26 07:59:24 +0800

    Add files via upload

---
+++ week3/3-2-4.c
@@ -0,0 +1,17 @@
+#include"ch02.h"
+int main()
+{
+FILE *fp;
+char buf[80];
+int ret;
+memset(buf,0,sizeof(buf));
+if((fp=fopen("./test1.txt","w"))==NULL)
+	perror("open failed!\n");
+printf("Please input string you want to store is file :\t");
+fgets(buf,sizeof(buf),stdin);
+printf("Content is %s. The size of stream is %ld bytes. \n",buf,sizeof(buf));
+ret=fwrite(buf,1,sizeof(buf),fp);
+printf("Actual writing number of item is %d\n",ret);
+fclose(fp);
+return 0;
+}
