ced79cf6d288b6cfb5df55be6c23e568a3752c44
liuxinzhe <72716124+liuxinzhe@users.noreply.github.com>
2020-10-26 08:00:23 +0800

    Add files via upload

---
+++ week4/4-6.c
@@ -0,0 +1,13 @@
+#include"ch03.h"
+int main()
+{
+	int d1;
+	d1=open("./out.info",O_CREAT|O_TRUNC|O_WRONLY,0644);
+
+	dup2(d1,1);
+
+	d1=dup(STDOUT_FILENO);
+	printf("Redirect stdout file!\n");
+	return 0;
+}
+ 
